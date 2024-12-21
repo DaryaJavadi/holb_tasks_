68f5f0559d0009fa36284ffa7dd18e56ccf8ae01
Darya Javadi <darya.javadi15@gmail.com>
2024-12-21 13:13:49 +0000

    fork

---
+++ simple_shell/fork.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int main(void)
+{
+	pid_t pid;
+
+	printf("Before fork\n");
+
+	pid = fork();
+
+	if (pid == -1)
+	{
+	perror("fork failed");
+	return 1;
+	}
+
+	printf("After fork\n");
+
+	pid_t my_pid = getpid();
+	printf("My pid is %d\n", my_pid);
+
+	if (pid == 0)
+	{
+	printf("(%d) Nooooooooo! I am the child.\n", my_pid);
+	}
+	else
+	{
+	printf("(%d) %d, I am your father.\n", my_pid, pid);
+	}
+
+	return 0;
+}
